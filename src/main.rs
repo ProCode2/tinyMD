91ccf0696f8b72495d0de4755897a8da7374907f
ProCode2 <bpro249@gmail.com>
2021-04-08 01:42:59 +0530

    tokenising markdown syntax

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,69 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
 fn parse_markdown_file(filename: &str) {
     print_banner();
-    println!("[ INFO ] Trying to parse {}...", filename)
+    println!("[ INFO ] Trying to parse {}...", filename);
+    let input_path = Path::new(filename);
+
+    let file = File::open(&input_path).expect("[ ERROR ] Failed to open file!");
+
+    let mut ptag: bool = false;
+    let mut htag: bool = false;
+
+    let mut tokens: Vec<String> = Vec::new();
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line_contents = line.unwrap();
+        let mut first_char: Vec<char> = line_contents.chars().take(1).collect();
+
+        let mut output_line = String::new();
+
+        match first_char.pop() {
+            Some('#') => {
+                if ptag {
+                    ptag = false;
+                    output_line.push_str("</p>\n");
+                }
+
+                if htag {
+                    htag = false;
+                    output_line.push_str("</h1>\n")
+                }
+
+                htag = true;
+                output_line.push_str("\n\n<h1>");
+                output_line.push_str(&line_contents[2..]);
+            }
+            _ => {
+                if !ptag {
+                    ptag = true;
+                    output_line.push_str("<p>");
+                }
+
+                output_line.push_str(&line_contents);
+            }
+        }
+
+        if ptag {
+            ptag = false;
+            output_line.push_str("</p>\n");
+        }
+
+        if htag {
+            htag = false;
+            output_line.push_str("</h1>\n");
+        }
+
+        if output_line != "<p></p>\n" {
+            tokens.push(output_line);
+        }
+    }
+
+    println!("{:?}", input_path)
 }
 
 fn get_version() -> String {
---
+++ test.md
@@ -0,0 +1,15 @@
+# My favorite author
+
+This is a report about my favorite writer. His name is Jesse Lawson.
+
+# Jesse's favorite food
+
+Jesse really likes enchiladas and any kind of sushi.
+
+# Jesse's favorite drink
+
+Jesse likes to drink coffee in the mornings and iced tea throughout the day. Sometimes, he even drinks water.
+
+# Jesse's favorite hobbies
+
+Jesse likes to write about computer programming and game design, and when he is not hunched over a computer, you can find him out on a run and listening to a podcast or the serenity of mother nature.
